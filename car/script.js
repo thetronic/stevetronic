876aa7c473d2676901d13b44e81a628f2fe642c9
thetronic <stevetronic@gmail.com>
2024-05-29 10:06:57 +0100

    Added Shuffle Function

--- car/script.js
+++ car/script.js
@@ -1,10 +1,28 @@
 const dataDiv = document.getElementById('gallery') // Replace with your div ID
 
+function shuffle(array) {
+  let currentIndex = array.length
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    const randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex--
+
+    // And swap it with the current element.
+    ;[array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ]
+  }
+}
+
 function loadData() {
   fetch('shows.json') // Replace "shows.json" with your actual file path
     .then((response) => response.json())
     .then((data) => {
       const dataDiv = document.getElementById('gallery')
+      shuffle(data)
       data.forEach((item) => {
         const image = document.createElement('img')
         console.log(item.image)
